f75c921718e805538008064efd21df5617dbdb68
Patrick O'Grady <ogradypatrickj@gmail.com>
2015-06-20 22:38:58 -0400

    Add Airbrake for error reporting

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,4 @@
 Airbrake.configure do |config|
-  config.api_key = "2d721e11ad6d407bce2770597145d806"
+  config.api_key = Rails.application.secrets.airbrake_api_key
+  config.environment_name = Rails.application.secrets.host
 end
